08963b195c29f1bf3e3c8f556cc2b4004ee42dac
Meaningfy <hi@meaningfy.ws>
2026-02-18 10:51:47 +0200

    chore: update public interfaces

--- src/ers/application/ports/__init__.py
+++ src/ers/application/ports/__init__.py
@@ -1,10 +1,18 @@
 from ers.application.ports.audit_log_repository import AuditLogRepository
+from ers.application.ports.canonical_entity_repository import (
+    CanonicalEntityRepository,
+)
 from ers.application.ports.decision_repository import DecisionRepository
+from ers.application.ports.entity_mention_repository import EntityMentionRepository
 from ers.application.ports.repositories import AsyncReadRepository, AsyncWriteRepository
+from ers.application.ports.statistics_repository import StatisticsRepository
 
 __all__ = [
     "AsyncReadRepository",
     "AsyncWriteRepository",
-    "DecisionRepository",
     "AuditLogRepository",
+    "CanonicalEntityRepository",
+    "DecisionRepository",
+    "EntityMentionRepository",
+    "StatisticsRepository",
 ]
