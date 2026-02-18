f37cf1c2a4005e76f307a3e4a462f88b76bab7db
Meaningfy <hi@meaningfy.ws>
2026-02-18 10:51:47 +0200

    chore: update public interfaces

--- src/ers/application/services/__init__.py
+++ src/ers/application/services/__init__.py
@@ -1,9 +1,15 @@
 from ers.application.services.audit_service import AuditService
+from ers.application.services.canonical_entity_service import CanonicalEntityService
 from ers.application.services.decision_curation_service import (
     DecisionCurationService,
 )
+from ers.application.services.entity_service import EntityService
+from ers.application.services.statistics_service import StatisticsService
 
 __all__ = [
     "AuditService",
+    "CanonicalEntityService",
     "DecisionCurationService",
+    "EntityService",
+    "StatisticsService",
 ]
