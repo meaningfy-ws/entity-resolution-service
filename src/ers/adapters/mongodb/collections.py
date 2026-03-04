4d61076e49d9c15ccc217aa246df18e3ea42bca9
Meaningfy <hi@meaningfy.ws>
2026-03-04 11:00:26 +0200

    feat: introduce centralized mongodb collections class

---
+++ src/ers/adapters/mongodb/collections.py
@@ -0,0 +1,30 @@
+from pymongo.asynchronous.collection import AsyncCollection
+from pymongo.asynchronous.database import AsyncDatabase
+
+
+class MongoCollections:
+    """Single source of truth for MongoDB collection names and access."""
+
+    DECISIONS = "decisions"
+    ENTITY_MENTIONS = "entity_mentions"
+    CANONICAL_ENTITIES = "canonical_entities"
+    USER_ACTIONS = "user_actions"
+
+    def __init__(self, database: AsyncDatabase) -> None:
+        self._db = database
+
+    @property
+    def decisions(self) -> AsyncCollection:
+        return self._db[self.DECISIONS]
+
+    @property
+    def entity_mentions(self) -> AsyncCollection:
+        return self._db[self.ENTITY_MENTIONS]
+
+    @property
+    def canonical_entities(self) -> AsyncCollection:
+        return self._db[self.CANONICAL_ENTITIES]
+
+    @property
+    def user_actions(self) -> AsyncCollection:
+        return self._db[self.USER_ACTIONS]
