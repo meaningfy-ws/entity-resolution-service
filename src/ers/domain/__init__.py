633ed46280876bbda44cbcb8d1b69594db81f085
Meaningfy <hi@meaningfy.ws>
2026-03-11 11:58:06 +0200

    chore: update domain public interface

--- src/ers/domain/__init__.py
+++ src/ers/domain/__init__.py
@@ -1,15 +1,22 @@
 from ers.domain.exceptions import (
     AlreadyCuratedError,
+    AuthenticationError,
+    AuthorizationError,
     DomainError,
     InvalidClusterError,
 )
 from ers.domain.models import UserActionFactory
+from ers.domain.user import User
 
 __all__ = [
     # Exceptions
     "DomainError",
     "AlreadyCuratedError",
+    "AuthenticationError",
+    "AuthorizationError",
     "InvalidClusterError",
+    # Domain models
+    "User",
     # Domain factories
     "UserActionFactory",
 ]
