1e3c9dc9e6e129c86f3f1d7bdc77eb1a84437732
Meaningfy <hi@meaningfy.ws>
2026-04-07 13:10:30 +0300

    chore: fix linting issue

--- src/ers/rdf_mention_parser/domain/rdf_mapping_config.py
+++ src/ers/rdf_mention_parser/domain/rdf_mapping_config.py
@@ -92,8 +92,8 @@ class RDFMappingConfig(BaseModel):
         """
         try:
             return self.entity_types[name]
-        except KeyError:
-            raise UnsupportedEntityTypeError(name)
+        except KeyError as e:
+            raise UnsupportedEntityTypeError(name) from e
 
     def resolve_entity_type(self, uri: str) -> EntityTypeConfig:
         """Return the EntityTypeConfig whose rdf_type expands to the given full URI.
